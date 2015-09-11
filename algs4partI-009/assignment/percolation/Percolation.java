3a0a6530342034391b1097f8d263e6adc236315b
wangjiezhe <wangjiezhe@gmail.com>
2015-09-11 13:28:27 +0800

    [A1]Add Assignment Percolation
    Get 92.50 / 100, for failing the backwash test.

---
+++ algs4partI-009/assignment/percolation/Percolation.java
@@ -0,0 +1,109 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+  private int N;
+  private WeightedQuickUnionUF wqu;
+  private int[] openStatus;
+  
+  /**
+   * create N-by-N grid, with all sites blocked
+   * plus two virtual site: 0 (over the top) and N*N+1 (below the bottom)
+   */
+  public Percolation(int N) {
+    if (N <= 0)
+      throw new IllegalArgumentException();
+    this.N = N;
+    wqu = new WeightedQuickUnionUF(N*N+2);
+    openStatus = new int[N*N+2];
+    for (int i = 0; i < N*N+2; i++)
+      openStatus[i] = 0;
+  }
+  
+  /**
+   * open site (row i, column j) if it is not open already
+   */
+  public void open(int i, int j) {
+    if (!isOpen(i, j)) {
+      if (i == 1) { // the top line
+        if (openStatus[0] == 0)
+          openStatus[0] = 1;
+        wqu.union(0, getLoc(i, j));
+      }
+      if (i == N) { // the bottom line
+        if (openStatus[N*N+1] == 0)
+          openStatus[N*N+1] = 1;
+        wqu.union(N*N+1, getLoc(i, j));
+      }
+      openStatus[getLoc(i, j)] = 1;
+      int[][] surround = {
+          {i-1, j}, {i+1, j}, {i, j-1}, {i, j+1}
+      };
+      for (int[] site:surround) {
+        try {
+          if (isOpen(site[0], site[1]))
+            wqu.union(getLoc(i, j), getLoc(site[0], site[1]));
+        } catch (IndexOutOfBoundsException e) {
+          continue;
+        }
+      }
+    }
+  }
+  
+  /**
+   * is site (row i, column j) open?
+   */
+  public boolean isOpen(int i, int j) {
+    return openStatus[getLoc(i, j)] == 1;
+  }
+  
+  /**
+   * is site (row i, column j) full?
+   * 
+   * A full site is an open site that can be connected to an open site
+   * in the top row via a chain of neighboring (left, right, up, down) open sites.
+   */
+  public boolean isFull(int i, int j) {
+    return wqu.connected(0, getLoc(i, j));
+  }
+  
+  /**
+   * does the system percolate?
+   * 
+   * We say the system percolates if there is a full site in the bottom row.
+   * In other words, a system percolates if we fill all open sites
+   * connected to the top row and that process fills some open site
+   * on the bottom row.
+   */
+  public boolean percolates() {
+      return wqu.connected(0, N*N+1);
+  }
+
+  /**
+   * test client (optional)
+   */
+  public static void main(String[] args) {
+     for (String arg : args) {
+      In in = new In(arg);
+      int N = in.readInt();
+      Percolation perc = new Percolation(N);
+      while (!in.isEmpty()) {
+        int i = in.readInt();
+        int j = in.readInt();
+        perc.open(i, j);
+      }
+      StdOut.println(perc.percolates());
+    }
+  }
+  
+  /**
+   * get the location of the site (row i, column j) in the array
+   * 
+   */
+  private int getLoc(int i, int j) {
+    if (i < 1 || i > N || j < 1 || j > N)
+      throw new IndexOutOfBoundsException();
+    return (i-1)*N+j;
+  }
+}
---
+++ algs4partI-009/assignment/percolation/PercolationStats.java
@@ -0,0 +1,71 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+  private double[] threshold;
+  
+  /**
+   * perform T independent experiments on an N-by-N grid
+   */
+  public PercolationStats(int N, int T) {
+    if (N <= 0 || T <= 0)
+      throw new IllegalArgumentException();
+    threshold = new double[T];
+    int[] percolationPoint = new int[T];
+    for (int i = 0; i < T; i++) {
+      percolationPoint[i] = 0;
+      Percolation perc = new Percolation(N);
+      while (!perc.percolates()) {
+        int j = StdRandom.uniform(1, N+1);
+        int k = StdRandom.uniform(1, N+1);
+        if (!perc.isOpen(j, k)) {
+          perc.open(j, k);
+          percolationPoint[i]++;
+        }
+      }
+      threshold[i] = percolationPoint[i] / (double) (N*N);
+    }
+  }
+  
+  /**
+   * sample mean of percolation threshold
+   */
+  public double mean() {
+    return StdStats.mean(threshold);
+  }
+  
+  /**
+   * sample standard deviation of percolation threshold
+   */
+  public double stddev() {
+    return StdStats.stddev(threshold);
+  }
+  
+  /**
+   * low endpoint of 95% confidence interval
+   */
+  public double confidenceLo() {
+    return mean() - 1.96 * stddev() / Math.sqrt(threshold.length);
+  }
+  
+  /**
+   * high endpoint of 95% confidence interval
+   */
+  public double confidenceHi() {
+    return mean() + 1.96 * stddev() / Math.sqrt(threshold.length);
+  }
+
+  /**
+   * test client (described below)
+   */
+  public static void main(String[] args) {
+    int N = Integer.parseInt(args[0]);
+    int T = Integer.parseInt(args[1]);
+    PercolationStats percs = new PercolationStats(N, T);
+    StdOut.println("mean                    = " + percs.mean());
+    StdOut.println("stddev                  = " + percs.stddev());
+    StdOut.println("%95 confidence interval = " +
+        percs.confidenceLo() + ", " + percs.confidenceHi());
+  }
+}
