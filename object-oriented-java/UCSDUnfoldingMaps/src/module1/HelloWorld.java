d6b0d3f01ed7b22831661689675eb89a92c31b03
wangjiezhe <wangjiezhe@gmail.com>
2015-09-19 21:44:28 +0800

    [OOPJ]Complete module 1

--- object-oriented-java/UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ object-oriented-java/UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -43,7 +43,7 @@ public class HelloWorld extends PApplet
 
 		// This sets the background color for the Applet.  
 		// Play around with these numbers and see what happens!
-		this.background(200, 200, 200);
+		this.background(255, 0, 0);
 		
 		// Select a map provider
 		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
@@ -74,16 +74,17 @@ public class HelloWorld extends PApplet
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
-		// TODO: Add code here that creates map2 
-		// Then you'll modify draw() below
-
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(39.81f, 98.27f));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 	}
 
 	/** Draw the Applet window.  */
 	public void draw() {
 		// So far we only draw map1...
-		// TODO: Add code so that both maps are displayed
+		// Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
